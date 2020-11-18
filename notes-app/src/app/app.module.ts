37b426431405878a71d366078e339d7062600e22
Djordje Milicevic <milicevic.djordje@hotmail.com>
2020-11-18 23:21:10 +0100

    Notes App: Animations
    Notes App: Animations

--- notes-app/src/app/app.module.ts
+++ notes-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +21,8 @@ import { NoteDetailsComponent } from './note-details/note-details.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
