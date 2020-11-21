fb71537d88c3f7ff5d2e2163aa0698b22d180356
Djordje Milicevic <milicevic.djordje@hotmail.com>
2020-11-21 23:30:06 +0100

    Dashboard: Header, Footer, Sidebar
    Dashboard: Header, Footer, Sidebar

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DefaultComponent } from './default/default.component';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DefaultComponent, children: [
+    { path: '', component: DashboardComponent },
+    { path: 'posts', component: PostsComponent }
+  ] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
