f3e0c4d3c680148d75889bb25bd673d4a658718e
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-03-04 13:00:13 +0100

    ViewChild, Local Reference, DOM, Lifecycle
    Udemy course Angular - The Complete Guide
    
    05 Components & Databinding Deep Dive - Done

--- my-first-app/src/app/cockpit/cockpit.component.ts
+++ my-first-app/src/app/cockpit/cockpit.component.ts
@@ -1,27 +1,29 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
-  styleUrls: ['./cockpit.component.css']
+  styleUrls: ['./cockpit.component.css'],
+  //encapsulation: ViewEncapsulation.None
 })
 export class CockpitComponent implements OnInit {
 
-  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output() blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();;
-  newServerName = '';
+  @Output('serverCreated') serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('blueprintCreated') blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();;
+  // newServerName = '';
   newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onAddServer() {
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddServer(serverNameInput: HTMLInputElement) {
+    this.serverCreated.emit({serverName: serverNameInput.value, serverContent: this.serverContentInput.nativeElement.value});
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({blueprintName: this.newServerName, blueprintContent: this.newServerContent});
+  onAddBlueprint(serverNameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({blueprintName: serverNameInput.value, blueprintContent: this.serverContentInput.nativeElement.value});
   }
 }
--- my-first-app/src/app/server-element/server-element.component.ts
+++ my-first-app/src/app/server-element/server-element.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
 })
 export class ServerElementComponent implements OnInit {
 
   @Input('element') element: {type: string, name: string, content: string};
 
-  constructor() { }
+  constructor() {
+    console.log('Constructor called');
+   }
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
   }
 
 }
