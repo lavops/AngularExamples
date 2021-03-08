e75cd3a3f2d8af4c0ee8710ea50fbba173491151
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-03-08 21:34:32 +0100

    Logging & Accounts Services
    Udemy course Angular - The Complete Guide
    
    09 Using Services & Dependency Injection - Done

--- my-first-app/src/app/new-account/new-account.component.ts
+++ my-first-app/src/app/new-account/new-account.component.ts
@@ -1,18 +1,22 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountsService } from '../services/accounts.service';
+import { LoggingService } from '../services/logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: []
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(
+    private accountsService: AccountsService
+  ) {
+    this.accountsService.statusUpdated.subscribe((status: string) => alert('New Status: ' + status));
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus);
   }
 }
