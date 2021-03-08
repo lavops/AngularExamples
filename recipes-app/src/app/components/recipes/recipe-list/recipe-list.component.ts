98fb06e229f27436ffb15e661cea1afe91d3536a
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-03-08 15:53:51 +0100

    Dropdown Directive
    Udemy course Angular - The Complete Guide
    
    08 Course Project - Directives - Done

--- recipes-app/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ recipes-app/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "This is simply a test", "https://www.blondelish.com/wp-content/uploads/2020/04/Keto-Stuffed-Cabbage-Rolls-Gluten-Free-Low-Carb-Recipe-10.jpg"),
+    new Recipe("Sarma", "Ovo je recept za sarmu", "https://www.blondelish.com/wp-content/uploads/2020/04/Keto-Stuffed-Cabbage-Rolls-Gluten-Free-Low-Carb-Recipe-10.jpg"),
   ];
   @Output() selectedRecipe = new EventEmitter<Recipe>();
 
---
+++ recipes-app/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+
+  @HostBinding('class.open') isOpen: boolean = false;
+
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+
+  constructor(
+    private renderer: Renderer2,
+    private elRef: ElementRef
+  ) { }
+
+  
+}
