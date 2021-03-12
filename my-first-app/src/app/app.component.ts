6f99cc73f207ba9b35836171001ed81a9b720a2e
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-03-12 20:15:28 +0100

    Reactive forms starting project
    Udemy course Angular - The Complete Guide
    
    15 Handling Forms in Angular Apps - Reactive Start

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,52 +6,5 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  @ViewChild('form') signupForm: NgForm;
-  defaultQuestion = 'pet';
-  answer = '';
   genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secret: '',
-    answer: '',
-    gender: ''
-  }
-  submitted = false;
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // this.signupForm.setValue({
-    //   userData: {
-    //     username: suggestedName,
-    //     email: ''
-    //   },
-    //   secret: 'pet',
-    //   questionAnswer: '',
-    //   gender: 'male'
-    // });
-
-    this.signupForm.form.patchValue({
-      userData:{
-        username: suggestedName
-      }
-    })
-  }
-
-  // onSubmit(event: NgForm) {
-  //   console.log(event.form.value);
-  // }
-
-  onSubmit() {
-    console.log(this.signupForm);
-    this.submitted = true;
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secret = this.signupForm.value.secret;
-    this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
-
-    this.signupForm.reset();
-  }
 }
