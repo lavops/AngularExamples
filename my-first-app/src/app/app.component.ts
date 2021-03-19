bcce8a38d0199bc7adc105f55bce39b5e50412e8
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-03-19 19:47:32 +0100

    Starting Template For Pipes
    Udemy course Angular - The Complete Guide
    
    17 Using Pipes to Transform Output - Starting Template

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,80 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  genders = ['male', 'female'];
-  signupForm: FormGroup;
-  forbiddenUsernames = ['Chris', 'Anna'];
-
-  ngOnInit() {
-    this.signupForm = new FormGroup({
-      'userData': new FormGroup({
-        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
-      }),      
-      'gender': new FormControl('male'),
-      'hobbies': new FormArray([])
-    });
-
-    // this.signupForm.valueChanges.subscribe((value) => {
-    //   console.log(value);
-    // })
-
-    // this.signupForm.statusChanges.subscribe((status) => {
-    //   console.log(status);
-    // })
-
-    this.signupForm.patchValue({
-      'userData': {
-        'username': 'Max'
-      }
-    })
-  }
-
-  onSubmit() {
-    console.log(this.signupForm);
-    this.signupForm.reset();
-  }
-
-  onAddHobby() {
-    const control = new FormControl(null, Validators.required);
-    (<FormArray>this.signupForm.get('hobbies')).push(control);
-  }
-
-  getControls() {
-    return (<FormArray>this.signupForm.get('hobbies')).controls;
-  }
-
-  // ILI OVO SA GETTEROM i samo bi bilo *ngFor="let hobbyControl of controls; let i = index"
-  // get controls() {
-  //   return (this.signupForm.get('hobbies') as FormArray).controls;
-  // }
-
-  forbiddenNames(control: FormControl): {[s: string] : boolean} {
-    if(this.forbiddenUsernames.indexOf(control.value) !== -1) {
-      return {'nameIsForbidden': true};
+export class AppComponent {
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
     }
-    return null;
-  }
-
-  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
-    const promise = new Promise<any>((resolve, reject) => {
-      setTimeout(() => {
-        if(control.value === 'test@test.com') {
-          resolve({'emailIsForbidden': true});
-        }
-        else {
-          resolve(null);
-        }
-      }, 1500);
-    });
-
-    return promise;
+  ];
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
   }
 }
