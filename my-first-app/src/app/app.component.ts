b3977c3573fa619645fe6e65072dd5a362893e6c
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-03-20 12:09:37 +0100

    Custom Pipe, Pure, Async
    Udemy course Angular - The Complete Guide
    
    17 Using Pipes to Transform Output - Done

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -32,11 +32,28 @@ export class AppComponent {
       started: new Date(15, 1, 2017)
     }
   ];
+
+  filteredStatus = '';
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    })
   }
 }
