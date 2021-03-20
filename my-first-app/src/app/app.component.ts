c83087f2ff25ef6a843071c830e90dc7c1d482c5
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-03-20 12:57:36 +0100

    Setting Up Firebase, Post and Get Functions
    Udemy course Angular - The Complete Guide
    
    17 Making HTTP Requests - Setting Up Firebase, Post and Get Functions

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +9,38 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {}
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
+    this.http.post('https://angularcoursehttprequests-default-rtdb.firebaseio.com/posts.json', postData).subscribe(response => {
+      console.log(response);
+    });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.http.get<{[key: string]: Post}>('https://angularcoursehttprequests-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map(response => {
+        const postsArray: Post[] = [];
+        for (const key in response) {
+          if(response.hasOwnProperty(key)){
+            postsArray.push({...response[key], id: key});
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe((posts: Post[]) => {
+        console.log(posts);
+        this.loadedPosts = posts;
+      })
   }
 
   onClearPosts() {
--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -6,8 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, 
+    FormsModule, 
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
