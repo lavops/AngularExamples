2f9c4e18017fee8438a4e2e3047618a6528cac90
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-03-23 14:18:48 +0100

    Extracting Components & Routes to Recipes Module
    Udemy course Angular - The Complete Guide
    
    22 Modules & Optimatizations - Extracting Components & Routes to Recipes Module and Recipes Routing Module

--- recipes-app/src/app/app-routing.module.ts
+++ recipes-app/src/app/app-routing.module.ts
@@ -1,22 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
-import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './components/recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './components/recipes/recipe-start/recipe-start.component';
-import { RecipesComponent } from './components/recipes/recipes.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
-import { AuthGuard } from './services/auth.guard';
-import { RecipeResolverService } from './services/recipe-resolver.service';
 
 const routes: Routes = [
   { path: "", redirectTo: "recipes", pathMatch: 'full' },
-  { path: "recipes", component: RecipesComponent , canActivate: [AuthGuard], children: [
-    { path: "", component: RecipeStartComponent },
-    { path: "new", component: RecipeEditComponent },
-    { path: ":id", component: RecipeDetailComponent, resolve: [RecipeResolverService] },
-    { path: ":id/edit", component: RecipeEditComponent, resolve: [RecipeResolverService] },
-  ] },
   { path: "shopping-list", component: ShoppingListComponent},
   { path: "auth", component: AuthComponent }
 ];
--- recipes-app/src/app/app.module.ts
+++ recipes-app/src/app/app.module.ts
@@ -5,17 +5,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { RecipesComponent } from './components/recipes/recipes.component';
-import { RecipeListComponent } from './components/recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './components/recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { RecipeService } from './services/recipe.service';
 import { ShoppingListService } from './services/shopping-list.service';
-import { RecipeStartComponent } from './components/recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './components/recipes/recipe-edit/recipe-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DataStorageService } from './services/data-storage.service';
 import { AuthComponent } from './components/auth/auth.component';
@@ -24,20 +18,15 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
 import { AuthInterceptorService } from './services/auth-interceptor.service';
 import { AuthGuard } from './services/auth.guard';
 import { AlertComponent } from './shared/alert/alert.component';
+import { RecipesModule } from './components/recipes/recipes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent
@@ -48,6 +37,7 @@ import { AlertComponent } from './shared/alert/alert.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    RecipesModule
   ],
   providers: [
     RecipeService,
