56f8105969ddda1e6481d930ab2ea2f236a63497
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-03-23 15:23:00 +0100

    Extracting Components & Routes to Auth Module and Auth  Routing Module
    Udemy course Angular - The Complete Guide
    
    22 Modules & Optimatizations - Extracting Components & Routes to Auth Module and Auth Routing Module

--- recipes-app/src/app/app.module.ts
+++ recipes-app/src/app/app.module.ts
@@ -5,17 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { AuthComponent } from './components/auth/auth.component';
 import { RecipesModule } from './components/recipes/recipes.module';
 import { ShoppingListModule } from './components/shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './components/auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,6 +22,7 @@ import { CoreModule } from './core.module';
     AppRoutingModule, 
     RecipesModule,
     ShoppingListModule,
+    AuthModule,
     SharedModule,
     CoreModule
   ],
